8c1dd587ac75533b96d06b75a6cfc37081c14f1f
oxfordblucher <hiuchanhk@gmail.com>
2020-10-06 18:28:40 -0400

    geocoding queries now functional

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 //On click should set variables for the input fields
 $("#submitAddresses").on("click", function(){
     console.log("Functioning");
-    let addressOne = $("#addressOne").val();
-    let addressTwo = $("#addressTwo").val();
-    alert(addressOne + addressTwo);
+    let numOne = $("#numOne").val();
+    let streetOne = $("#streetOne").val();
+    let cityOne = $("#cityOne").val();
+    let stateOne = $("#stateOne").val();
+    let codeOne = $("#codeOne").val();
+    let numTwo = $("#numTwo").val();
+    let streetTwo = $("#streetTwo").val();
+    let cityTwo = $("#cityTwo").val();
+    let stateTwo = $("#stateTwo").val();
+    let codeTwo = $("#codeTwo").val();
+
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://forward-reverse-geocoding.p.rapidapi.com/v1/forward?polygon_geojson=0&state=" + stateOne + "&limit=1&street=" + streetOne + "&polygon_svg=0&postalcode=" + codeOne + 
+        "&polygon_kml=0&namedetails=0&accept-language=en&city=" + cityOne + "&addressdetails=1&polygon_threshold=0.0&polygon_text=0&bounded=0&format=json",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "forward-reverse-geocoding.p.rapidapi.com",
+            "x-rapidapi-key": "d5a9e5282bmshadfe616217dbda8p1fd537jsn689affc185cc"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 });
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://forward-reverse-geocoding.p.rapidapi.com/v1/forward?polygon_geojson=0&state=NY&limit=5&street=12%20West%204th%20Street&polygon_svg=0&country=USA&polygon_kml=0&namedetails=0&accept-language=en&city=New%20York&addressdetails=1&polygon_threshold=0.0&polygon_text=0&bounded=0&format=json",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "forward-reverse-geocoding.p.rapidapi.com",
-		"x-rapidapi-key": "d5a9e5282bmshadfe616217dbda8p1fd537jsn689affc185cc"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
