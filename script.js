a1ddb2d0774966c4d5786b932b40c5ce030da047
Claire Vita <clairej.vita@gmail.com>
2020-10-08 19:57:51 -0400

    added map to div

--- script.js
+++
@@ -1,7 +0,0 @@
-//On click should set variables for the input fields
-$("#submitAddresses").on("click", function(){
-    console.log("Functioning");
-    let addressOne = $("#addressOne").val();
-    let addressTwo = $("#addressTwo").val();
-    alert(addressOne + addressTwo);
-});
